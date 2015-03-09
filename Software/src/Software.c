de549b06706d69ea2ed7ac915b6bca0f9d6dd5bc
Marcus Rådman <marrdm-9@student.ltu.se>
2015-03-09 12:05:30 +0100

    Improved pwm

--- Software/src/Software.c
+++ Software/src/Software.c
@@ -22,26 +22,22 @@ int main(void) {
 	LPC_GPIO1->DIR |= (1<<2);
 	LPC_GPIO0->DIR |= (1<<11);
 
-	LPC_SYSCON->SYSAHBCLKCTRL |= (1<<10);		//Power up tmr0
-	LPC_IOCON->R_PIO1_1 = 3;
+	pwm_init();
 
-	LPC_TMR32B1->PR = 28;
-	LPC_TMR32B1->MCR |= (1<<10);
-	LPC_TMR32B1->MR3 = 255;
-	LPC_TMR32B1->MR0 = 256;
-
-	LPC_TMR32B1->PWMC = (1<<0);
-	LPC_TMR32B1->PWMC |= (1<<3);
-
-	LPC_TMR32B1->TCR = 0x2;		//Reset timer
-	LPC_TMR32B1->TCR = 0x1;		//Start timer
-
-
-    // Force the counter to be placed into memory
-    volatile static int i = 0 ;
-    // Enter an infinite loop, just incrementing a counter
+	int k = 1;
+	int j = 0;
     while(1) {
-        i++ ;
+    	int i = 0;
+    	while(i<10000){
+    		i++;
+    	}
+    	j = j + k;
+    	pwm_red(j);
+//    	pwm_blue(j);
+    	pwm_green(255 - j);
+    	if(j > 255 || j <= 0){
+    		k = - k;
+    	}
     }
     return 0 ;
 }
--- Software/src/pwm.c
+++ Software/src/pwm.c
@@ -7,24 +7,65 @@
 
 #include "pwm.h"
 
-void pwm_init(LPC_TMR_TypeDef timer, int frequency, int reset_channel){
-//	int prescale = SYSTEM_CLOCK / (frequency * 1000);
-/*
-	LPC_SYSCON->SYSAHBCLKCTRL |= (1<<9);		//Power up tmr0
+//32B1_MAT0		RED
+//32B1_MAT1		GREEN
+//32B0_MAT3		BLUE
+
+void pwm_init(){
+	LPC_SYSCON->SYSAHBCLKCTRL |= (3<<9);		//Power up CT32B0 and CT32B0
+
 	LPC_IOCON->R_PIO1_1 = 3;
+	LPC_IOCON->R_PIO1_2 = 3;
+	LPC_IOCON->R_PIO0_11 = 3;
 
-	timer->PR = 28;//prescale;
-	timer->MCR |= (1<<10);
-	timer->MR3 = 255;
-	timer->MR0 = 128;
+	LPC_TMR32B1->PR = 28;						//set prescaler
+	LPC_TMR32B1->MCR |= (1<<10);				//set timer to reset on MR3
+	LPC_TMR32B1->MR3 = 255;
+	LPC_TMR32B1->MR0 = 255;
+	LPC_TMR32B1->MR1 = 255;
 
-	timer->PWMC = 1;
+	LPC_TMR32B1->PWMC |= (1<<0);				//set MR0 as PWM output
+	LPC_TMR32B1->PWMC |= (1<<1);				//set MR1 as PWM output
 
-	timer->TCR = 0x2;		//Reset timer
-	timer->TCR = 0x1;		//Start timer
-*/
+	LPC_TMR32B1->TCR = 0x2;						//Reset CT32B1
+	LPC_TMR32B1->TCR = 0x1;						//Start CT32B1
+
+
+	LPC_TMR32B0->PR = 28;						//set prescaler
+	LPC_TMR32B0->MCR |= (1<<1);					//set timer to reset on MR0
+	LPC_TMR32B0->MR0 = 255;
+	LPC_TMR32B0->MR3 = 255;
+
+	LPC_TMR32B0->PWMC |= (1<<3);				//set MR3 as PWM output
+
+	LPC_TMR32B0->TCR = 0x2;						//Reset CT32B1
+	LPC_TMR32B0->TCR = 0x1;						//Start CT32B1
 }
 
-void pwm_enable_output(int channel){
-
+void pwm_red(int value){
+	if(value < 0){
+		value = 0;
+	}else if(value > 255){
+		value = 255;
+	}
+	LPC_TMR32B1->MR0 = 255 - value;
 }
+
+void pwm_green(int value){
+	if(value < 0){
+		value = 0;
+	}else if(value > 255){
+		value = 255;
+	}
+	LPC_TMR32B0->MR3 = 255 - value;
+}
+
+void pwm_blue(int value){
+	if(value < 0){
+		value = 0;
+	}else if(value > 255){
+		value = 255;
+	}
+	LPC_TMR32B1->MR1 = 255 - value;
+}
+
