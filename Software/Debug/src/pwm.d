e171c17b0dbf5ba765985dcbc4e373bf8a9c93ae
jefsar-4 <jefsar-4@student.ltu.se>
2015-03-13 16:07:06 +0100

    Added interrupt code for timer, UART, button and AD

--- Software/Debug/src/pwm.d
+++ Software/Debug/src/pwm.d
@@ -1,18 +1,18 @@
 src/pwm.o src/pwm.d: ../src/pwm.c ../src/pwm.h \
- C:\Users\Marcus\Dropbox\E7020E\ (LP3\ 14-15)\Project\CMSISv2p00_LPC11xx\inc/LPC11xx.h \
- C:\Users\Marcus\Dropbox\E7020E\ (LP3\ 14-15)\Project\CMSISv2p00_LPC11xx\inc/core_cm0.h \
- C:\Users\Marcus\Dropbox\E7020E\ (LP3\ 14-15)\Project\CMSISv2p00_LPC11xx\inc/core_cmInstr.h \
- C:\Users\Marcus\Dropbox\E7020E\ (LP3\ 14-15)\Project\CMSISv2p00_LPC11xx\inc/core_cmFunc.h \
- C:\Users\Marcus\Dropbox\E7020E\ (LP3\ 14-15)\Project\CMSISv2p00_LPC11xx\inc/system_LPC11xx.h
+ C:\Users\jeffrey\Documents\E7020E\SmartThermometer\CMSISv2p00_LPC11xx\inc/LPC11xx.h \
+ C:\Users\jeffrey\Documents\E7020E\SmartThermometer\CMSISv2p00_LPC11xx\inc/core_cm0.h \
+ C:\Users\jeffrey\Documents\E7020E\SmartThermometer\CMSISv2p00_LPC11xx\inc/core_cmInstr.h \
+ C:\Users\jeffrey\Documents\E7020E\SmartThermometer\CMSISv2p00_LPC11xx\inc/core_cmFunc.h \
+ C:\Users\jeffrey\Documents\E7020E\SmartThermometer\CMSISv2p00_LPC11xx\inc/system_LPC11xx.h
 
 ../src/pwm.h:
 
-C:\Users\Marcus\Dropbox\E7020E\ (LP3\ 14-15)\Project\CMSISv2p00_LPC11xx\inc/LPC11xx.h:
+C:\Users\jeffrey\Documents\E7020E\SmartThermometer\CMSISv2p00_LPC11xx\inc/LPC11xx.h:
 
-C:\Users\Marcus\Dropbox\E7020E\ (LP3\ 14-15)\Project\CMSISv2p00_LPC11xx\inc/core_cm0.h:
+C:\Users\jeffrey\Documents\E7020E\SmartThermometer\CMSISv2p00_LPC11xx\inc/core_cm0.h:
 
-C:\Users\Marcus\Dropbox\E7020E\ (LP3\ 14-15)\Project\CMSISv2p00_LPC11xx\inc/core_cmInstr.h:
+C:\Users\jeffrey\Documents\E7020E\SmartThermometer\CMSISv2p00_LPC11xx\inc/core_cmInstr.h:
 
-C:\Users\Marcus\Dropbox\E7020E\ (LP3\ 14-15)\Project\CMSISv2p00_LPC11xx\inc/core_cmFunc.h:
+C:\Users\jeffrey\Documents\E7020E\SmartThermometer\CMSISv2p00_LPC11xx\inc/core_cmFunc.h:
 
-C:\Users\Marcus\Dropbox\E7020E\ (LP3\ 14-15)\Project\CMSISv2p00_LPC11xx\inc/system_LPC11xx.h:
+C:\Users\jeffrey\Documents\E7020E\SmartThermometer\CMSISv2p00_LPC11xx\inc/system_LPC11xx.h:
--- Software/Debug/src/subdir.mk
+++ Software/Debug/src/subdir.mk
@@ -26,7 +26,7 @@ C_DEPS += \
 src/%.o: ../src/%.c
 	@echo 'Building file: $<'
 	@echo 'Invoking: MCU C Compiler'
-	arm-none-eabi-gcc -DDEBUG -D__CODE_RED -DCORE_M0 -D__USE_CMSIS=CMSISv2p00_LPC11xx -D__LPC11XX__ -D__REDLIB__ -I"C:\Users\Marcus\Dropbox\E7020E (LP3 14-15)\Project\CMSISv2p00_LPC11xx\inc" -O0 -g3 -Wall -c -fmessage-length=0 -fno-builtin -ffunction-sections -fdata-sections -mcpu=cortex-m0 -mthumb -specs=redlib.specs -MMD -MP -MF"$(@:%.o=%.d)" -MT"$(@:%.o=%.o)" -MT"$(@:%.o=%.d)" -o "$@" "$<"
+	arm-none-eabi-gcc -DDEBUG -D__CODE_RED -DCORE_M0 -D__USE_CMSIS=CMSISv2p00_LPC11xx -D__LPC11XX__ -D__REDLIB__ -I"C:\Users\jeffrey\Documents\E7020E\SmartThermometer\CMSISv2p00_LPC11xx\inc" -O0 -g3 -Wall -c -fmessage-length=0 -fno-builtin -ffunction-sections -fdata-sections -mcpu=cortex-m0 -mthumb -specs=redlib.specs -MMD -MP -MF"$(@:%.o=%.d)" -MT"$(@:%.o=%.o)" -MT"$(@:%.o=%.d)" -o "$@" "$<"
 	@echo 'Finished building: $<'
 	@echo ' '
 
