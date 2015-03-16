e21d3ab213339ec14415a63a83c94497809ef449
Marcus Rådman <marrdm-9@student.ltu.se>
2015-03-16 11:46:31 +0100

    Fixed timer driven ADC

--- Software/Debug/src/Software.d
+++ Software/Debug/src/Software.d
@@ -3,8 +3,7 @@ src/Software.o src/Software.d: ../src/Software.c ../src/Software.h \
  C:\Users\Marcus\Dropbox\E7020E\ (LP3\ 14-15)\Project\CMSISv2p00_LPC11xx\inc/core_cm0.h \
  C:\Users\Marcus\Dropbox\E7020E\ (LP3\ 14-15)\Project\CMSISv2p00_LPC11xx\inc/core_cmInstr.h \
  C:\Users\Marcus\Dropbox\E7020E\ (LP3\ 14-15)\Project\CMSISv2p00_LPC11xx\inc/core_cmFunc.h \
- C:\Users\Marcus\Dropbox\E7020E\ (LP3\ 14-15)\Project\CMSISv2p00_LPC11xx\inc/system_LPC11xx.h \
- ../src/pwm.h
+ C:\Users\Marcus\Dropbox\E7020E\ (LP3\ 14-15)\Project\CMSISv2p00_LPC11xx\inc/system_LPC11xx.h
 
 ../src/Software.h:
 
@@ -17,5 +16,3 @@ C:\Users\Marcus\Dropbox\E7020E\ (LP3\ 14-15)\Project\CMSISv2p00_LPC11xx\inc/core
 C:\Users\Marcus\Dropbox\E7020E\ (LP3\ 14-15)\Project\CMSISv2p00_LPC11xx\inc/core_cmFunc.h:
 
 C:\Users\Marcus\Dropbox\E7020E\ (LP3\ 14-15)\Project\CMSISv2p00_LPC11xx\inc/system_LPC11xx.h:
-
-../src/pwm.h:
